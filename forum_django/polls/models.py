72cda218cd07b8e036a52082055d4cb133dc8756
arturoromeroum <ajruwork@gmail.com>
2020-08-19 12:22:51 -0500

    se mejora el app

--- forum_django/polls/models.py
+++ forum_django/polls/models.py
@@ -1,6 +1,7 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django import forms
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
